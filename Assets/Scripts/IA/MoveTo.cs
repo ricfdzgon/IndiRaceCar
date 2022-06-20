b77c17639c73cd1b6c4d6e6584e633ed3b79df33
Ricardo Fernández <ricfdzgon@gmail.com>
2022-06-20 23:03:40 +0200

    Seguimiento IA

--- Assets/Scripts/IA/MoveTo.cs
+++ Assets/Scripts/IA/MoveTo.cs
@@ -6,11 +6,31 @@ using UnityEngine.AI;
 
 public class MoveTo : MonoBehaviour
 {
+    public List<Transform> waypoints = new List<Transform>();
     public Transform goal;
+    private int currentWaypoint = 0;
+    private int maxWaypoints = 0;
+    private NavMeshAgent agent;
 
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        agent = GetComponent<NavMeshAgent>();
+        maxWaypoints = waypoints.Count;
     }
+
+    void Update()
+    {
+        agent.destination = waypoints[currentWaypoint].position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Waypoint")
+        {
+            currentWaypoint++;
+            Debug.Log(currentWaypoint);
+            Debug.Log(maxWaypoints);
+        }
+    }
+
 }
